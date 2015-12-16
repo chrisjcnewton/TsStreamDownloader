3e7d07410796d5657ac01d0138ca7196acd462db
Chris Newton <cnewton@tagworldwide.com>
2015-12-16 17:46:54 +0000

    added mac menu bar

--- src/js/appMenuBar.js
+++ src/js/appMenuBar.js
@@ -8,35 +8,36 @@ var win = gui.Window.get();
 var menubar = new gui.Menu({ type: 'menubar' });
 
 if (process.platform === "darwin") {  // this should indicate you're on Mac OSX
-  //menubar.createMacBuiltin(gui.App.manifest.description);
+  menubar.createMacBuiltin(gui.App.manifest.description);
 }
 
-// Create a menuitem
-var fileMenu = new gui.Menu();
 
-
-fileMenu.append(new gui.MenuItem({
-  type: 'normal',
-  label: 'Check for new content',
-  click: function () {
-      gui.Window.get().close(true);
-  }
-}));
-
-fileMenu.append(new gui.MenuItem({
-  type: 'separator'
-}));
-
-fileMenu.append(new gui.MenuItem({
-  type: 'normal',
-  label: 'Exit',
-  click: function () {
-      gui.Window.get().close(true);
-  }
-}));
-
-// You can have submenu!
-menubar.append(new gui.MenuItem({ label: 'File', submenu: fileMenu}));
+// // Create a menuitem
+// var fileMenu = new gui.Menu();
+//
+//
+// fileMenu.append(new gui.MenuItem({
+//   type: 'normal',
+//   label: 'Check for new content',
+//   click: function () {
+//       gui.Window.get().close(true);
+//   }
+// }));
+//
+// fileMenu.append(new gui.MenuItem({
+//   type: 'separator'
+// }));
+//
+// fileMenu.append(new gui.MenuItem({
+//   type: 'normal',
+//   label: 'Exit',
+//   click: function () {
+//       gui.Window.get().close(true);
+//   }
+// }));
+//
+// // You can have submenu!
+// menubar.append(new gui.MenuItem({ label: 'File', submenu: fileMenu}));
 
 //assign the menubar to window menu
 win.menu = menubar;
